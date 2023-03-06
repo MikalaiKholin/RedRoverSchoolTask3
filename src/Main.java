470966fb9f108fbf7d5a753b36eb5289f9944cec
MikalaiKholin <kholin.mikalai@gmail.com>
2023-03-07 00:24:42 +0300

    init: start task3

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         if (a < b) System.out.println("a < b");
         if (a > b) System.out.println("a > b");
-        if (a == b)  System.out.println("a > b");
+        if (a == b) System.out.println("a > b");
 
         if ( (a + b) % 2 == 0) {System.out.println("maybe a and b are even");}
                 else {System.out.println("some variable is odd");}
